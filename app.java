046dba7bd2a08d68917a927942892b5f9d157106
santhoshkumars1 <santhoshkumars@paigeon.com>
2024-08-12 12:28:34 +0530

    added play funtion

--- app.java
+++ app.java
@@ -1,5 +1,9 @@
 class app{
     public static void main(String[]main){
         System.out.println("this is java app");
+        play();
+    }
+    public static void play(){
+        System.out.println("playing...");
     }
 }
